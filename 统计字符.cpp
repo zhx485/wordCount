464b8565ed26708dcc71acbcbb91c764a3d40b61
zhx485 <54971988+zhx485@users.noreply.github.com>
2019-10-19 10:48:05 +0800

    Add files via upload

---
+++ 统计字符.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<cstdio>
+#include<fstream>
+using namespace std;
+string a[10];
+int tongjizifu(string h){
+	int k;
+    int sum=0;
+	char filename[10] ; //文件名
+	for (k = 0; k < h.length(); k++) //string => char[]
+	{
+		filename[k] = h[k];
+	}
+	filename[k] = '\0';
+	FILE* fp;
+	fp = fopen(filename, "a+"); //判断文件是否存在及可读
+	char jk;
+/*	while(1){
+	fscanf(fp,"%c",&jk);
+	if (jk=='\0') break;
+	else sum++;
+	}*/
+	int i=0 ,j=0;
+	while(1){
+	fscanf(fp, "%c", &a[i][j]);  
+	if (a[i][j]=='\0') break;
+	if (a[i][j]=='\n'){
+		i++;
+		j=0;
+	}
+	else j++;
+}
+	fclose(fp);
+
+	i=0,j=0;
+	while(1)
+	{
+		if (a[i][j]!=0)sum++;//统计字符数 
+		else break;
+		if (a[i][j]=='\n') {
+		i++;
+		j=0;
+	}
+		else j++;
+	}
+	return sum;
+}
+int main(){
+	int m;
+    cout<<tongjizifu("input.txt");
+    return 0;
+}
