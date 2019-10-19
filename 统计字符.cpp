c796bba04c2634cc0eaa824c7b4da4b829f8786a
zhx485 <54971988+zhx485@users.noreply.github.com>
2019-10-19 11:12:52 +0800

    Update 统计字符.cpp

--- 统计字符.cpp
+++ 统计字符.cpp
@@ -6,7 +6,7 @@ string a[10];
 int tongjizifu(string h){
 	int k;
     int sum=0;
-	char filename[10] ; //ÎÄ¼şÃû
+	char filename[10] ; //æ–‡ä»¶å
 	for (k = 0; k < h.length(); k++) //string => char[]
 	{
 		filename[k] = h[k];
@@ -22,33 +22,6 @@ int tongjizifu(string h){
 		sum++;
 	}
 	ax.close();
-	/*
-	FILE* fp;
-	fp = fopen(filename, "a+"); //ÅĞ¶ÏÎÄ¼şÊÇ·ñ´æÔÚ¼°¿É¶Á
-	char jk;
-	int i=0 ,j=0;
-	while(1){
-	fscanf(fp, "%c", &a[i][j]);  
-	if (a[i][j]=='\0') break;
-	if (a[i][j]=='\n'){
-		i++;
-		j=0;
-	}
-	else j++;
-}
-	fclose(fp);
-
-	i=0,j=0;
-	while(1)
-	{
-		if (a[i][j]!=0)sum++;//Í³¼Æ×Ö·ûÊı 
-		else break;
-		if (a[i][j]=='\n') {
-		i++;
-		j=0;
-	}
-		else j++;
-	}*/
 	return sum;
 }
 int main(){
